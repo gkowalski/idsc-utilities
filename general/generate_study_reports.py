0527316886006b989df7f42681bc93be6b456c6e
George Kowalski <gkowalski@mcw.edu>
2022-09-28 08:59:27 -0500

    new file

---
+++ general/generate_study_reports.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+print ( "Starting generate_Study_reports ")
+# Generate a CSV Extract of all studies
+## Users  https://xnat.mcw.edu/xnat/data/users?format=csv
+## Projects https://xnat.mcw.edu/xnat/data/projects?format=csv
+# Pull list of active accounts from XNAT
+
+# Email CSV it to this  list
+print ( "Ending generate_Study_reports ")
